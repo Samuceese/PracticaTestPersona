b8e7452915dd800c8c5fbcd3e93e7f3a7659e8d9
Samuel Cortés Sánchez <samuelcortessanchez@gmail.com>
2024-09-18 23:16:19 +0200

    Funcion con excepciones

--- PersonaTest/src/main/kotlin/validators/DniValidator.kt
+++ PersonaTest/src/main/kotlin/validators/DniValidator.kt
@@ -2,21 +2,31 @@ package validators
 
 class DniValidator {
 
-    fun validarDni(dni: String): Boolean {
-        val regex = Regex("[0-9]{8}[A-Z]")
+    class InvalidDniFormatException(message: String) : Exception(message)
+    class InvalidDniLetterException(message: String) : Exception(message)
 
-        if (!dni.matches(regex)) {
-            return false
+    class DniValidator {
+
+        fun validarDni(dni: String): Boolean {
+            val regex = Regex("[0-9]{8}[A-Z]")
+
+            if (!dni.matches(regex)) {
+                throw InvalidDniFormatException("El formato del DNI es incorrecto. Debe tener 8 números y 1 letra mayúscula.")
+            }
+
+            val numerosDni = dni.slice(0..7).toInt()
+            val letraDni = dni.slice(8..8).uppercase()
+
+            val letrasDni = "TRWAGMYFPDXBNJZSQVHLCKE"
+            val resto = (numerosDni % 23)
+            val letraCorrecta = letrasDni[resto].toString()
+
+            if (letraDni != letraCorrecta) {
+                throw InvalidDniLetterException("La letra del DNI es incorrecta. Se esperaba $letraCorrecta.")
+            }
+
+            return true
         }
-
-        val numerosDni = dni.slice(0..7).toInt()
-        val letraDni = dni.slice(8..8).uppercase()
-
-        val letrasDni = "TRWAGMYFPDXBNJZSQVHLCKE"
-
-        val resto = (numerosDni % 23)
-        val letraCorrecta = letrasDni[resto].toString()
-
-        return letraDni == letraCorrecta
     }
+
 }
